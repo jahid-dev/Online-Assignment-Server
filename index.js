4522572fd52c68b2dc6035caa9b02d7c88589b91
zinolot <mrjahidhassan0@gmail.com>
2023-11-06 18:06:07 +0600

    single assignment route created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
 require("dotenv").config();
 
@@ -47,6 +47,13 @@ async function run() {
       res.send(result)
     })
 
+    //view single assignment
+    app.get('/api/v1/allassignments/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await allAssignmentsCollection.findOne(query)
+      res.send(result)
+    })
     
    
 
